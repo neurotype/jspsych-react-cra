82915c8320e5a29d689044f6834b9fef08d10207
Scott Burwell <scott.j.burwell@gmail.com>
2021-08-11 14:14:21 -0500

    initialized with create-react-app, sintegrated jspsych-react code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import jsPsych from "jspsych";
+import plugins from "./plugins"; // load all built-in plugins
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class ExperimentWindow extends Component {
+  constructor(props) {
+    super(props);
+    const default_timeline = [
+      {
+        type: "html-keyboard-response",
+        stimulus: "This is the default timeline."
+      }
+    ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    Object.assign(jsPsych.plugins, plugins, props.plugins);
+
+    this.handleKeyEvent = e => {
+      if (e.redispatched) {
+        return;
+      }
+      let new_event = new e.constructor(e.type, e);
+      new_event.redispatched = true;
+      this.experimentDiv.dispatchEvent(new_event);
+    };
+
+    this.experimentDiv = null;
+    this.width = props.width || "100%";
+    this.height = props.height || "100%";
+
+    this.settings = {
+      ...props.settings,
+      timeline: props.settings.timeline || default_timeline
+    };
+  }
+
+  render() {
+    return (
+      <div
+        id="experimentWindow"
+        style={{ height: this.height, width: this.width }}
+        ref={e => {
+          this.experimentDiv = e;
+        }}
+      />
+    );
+  }
+
+  componentDidMount() {
+    window.addEventListener("keyup", this.handleKeyEvent, true);
+    window.addEventListener("keydown", this.handleKeyEvent, true);
+    jsPsych.init({ ...this.settings, display_element: this.experimentDiv });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keyup", this.handleKeyEvent, true);
+    window.removeEventListener("keydown", this.handleKeyEvent, true);
+    try {
+      jsPsych.endExperiment("Ended Experiment");
+    } catch (e) {
+      console.log("Experiment closed before unmount");
+    }
+  }
+}
+
+export { jsPsych, ExperimentWindow };
