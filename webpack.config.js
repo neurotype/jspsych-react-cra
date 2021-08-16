ae013648803f27e6b127012faadfb1f26f5c6cec
Scott Burwell <scott.j.burwell@gmail.com>
2021-08-16 12:18:06 -0500

    successfully compiles ./dist/jspsych-react-cra

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
   },
   output: {
-    filename: "build/static/js/WidgetCL.js",
+    filename: "jspsych-react-cra.js",
   },
   module: {
     rules: [
@@ -19,7 +19,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: ['@babel/plugin-transform-runtime']
           }
         }
